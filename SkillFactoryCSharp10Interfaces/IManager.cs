14c3cf4281489a033e8de206fa9b547912d3560b
ɐllɐ <molchanova.a.a@gmail.com>
2024-10-17 15:32:06 +0300

    task 10.1.4

---
+++ SkillFactoryCSharp10Interfaces/IManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SkillFactoryCSharp10Interfaces
+{
+	public interface IManager
+	{
+		public void Create();
+        public void Read();
+        public void Update();
+        public void Delete();
+
+    }
+}
+
---
+++ SkillFactoryCSharp10Interfaces/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SkillFactoryCSharp10Interfaces
+{
+    public class Manager : IManager
+    {
+        public void Create()
+        {
+
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+}
